d431b13be4845690657bdcd432db64741d61e0ea
Mariusz-Magiera <235860@student.pwr.edu.pl>
2019-05-02 19:33:15 +0200

    removed unnecessary lines of code

--- src/main/java/com/gamification/server/ServerApplication.java
+++ src/main/java/com/gamification/server/ServerApplication.java
@@ -1,14 +1,11 @@
 package com.gamification.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackages = "com.gamification.server.repository")
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class ServerApplication {
 
     public static void main(String[] args) {
