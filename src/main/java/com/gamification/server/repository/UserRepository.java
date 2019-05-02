36b0205a28a677b12d7d4b1e10d30362b739f065
Mariusz-Magiera <235860@student.pwr.edu.pl>
2019-05-02 19:38:35 +0200

    query for finding the user by name & password

--- src/main/java/com/gamification/server/repository/UserRepository.java
+++ src/main/java/com/gamification/server/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.gamification.server.repository;
 import com.gamification.server.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByNameAndPassword(String name, String password);
 }
