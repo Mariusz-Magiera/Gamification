bf106c999fca1256b5649bb675d394d749f9ef2f
Mariusz-Magiera <235860@student.pwr.edu.pl>
2019-05-02 19:36:25 +0200

    API for listing profile links

--- src/main/java/com/gamification/server/resources/UserResource.java
+++ src/main/java/com/gamification/server/resources/UserResource.java
@@ -1,13 +1,14 @@
 package com.gamification.server.resources;
 
+import com.gamification.server.model.ProfileLink;
 import com.gamification.server.model.User;
+import com.gamification.server.repository.ProfileLinkRepository;
 import com.gamification.server.repository.UserRepository;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,13 +19,27 @@ public class UserResource {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    ProfileLinkRepository profileLinkRepository;
+
     @GetMapping({"/all", "/", ""})
     public List<User> getAll(){
         return userRepository.findAll();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/id/{id}")
     public Optional<User> getUserById(@PathVariable final Integer id){
         return userRepository.findById(id);
     }
+
+    @GetMapping("/id/{id}/links")
+    public List<ProfileLink> getLinks(@PathVariable final Integer id){
+        Optional<User> user = getUserById(id);
+        if(user.isPresent()){
+            return new ArrayList<>(user.get().getLinks());
+        }
+        return null;
+    }
+
+    
 }
